1216893c785da10500d45475766bbf9091e67838
alfathony <email.alfathony@gmail.com>
2023-03-09 14:51:31 +0700

    fitst initiate commits

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div style="height:100%" class="w-full h-screen">
+    <iframe class="w-full h-screen" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FI5SRqCHa36qKeQ5sXvWnhB%2FKredivo-Design-System%3Fpage-id%3D3963%253A34741%26node-id%3D3963%253A34742%26viewport%3D838%252C557%252C0.79%26scaling%3Dscale-down" allowfullscreen></iframe>
+  </div>
+</template>
